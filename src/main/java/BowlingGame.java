3cc734953d6bef4f2c48adac3c5a2ed4b6d69f60
Rewinder <ren2935872@126.com>
2017-05-04 16:11:37 +0800

    add | some const var of the game add | some methods to sub strings

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,15 @@
 public class BowlingGame {
 
+    private final static int GAME_ROUND = 10;
+    private final static char STRIKE = 'X';
+    private final static char SPARE = '/';
+    private final static char MISS = '-';
+    private final static String BOUNDARY_NORMAL = "\\|";
+    private final static String BOUNDARY_EXTRA = "\\|\\|";
     public int getBowlingScore(String bowlingCode) {
+        String[] bowlingGrid = bowlingCode.split(BOUNDARY_EXTRA);
+        int extraNum = bowlingGrid.length - 1;
+        String[] normalBowlingGrid = bowlingGrid[0].split(BOUNDARY_NORMAL);
         return 0;
     }
 }
