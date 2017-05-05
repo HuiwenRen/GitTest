8cc06d1755bb92f2ee523f017186be1d5a0b5c43
Rewinder <ren2935872@126.com>
2017-05-05 14:16:30 +0800

    add | method for extra grids

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,23 +1,32 @@
 public class BowlingGame {
 
-    private final int  GAME_ROUND = 10;
-    private final int  BOWLING_NUM = 10;
-    private final char STRIKE = 'X';
-    private final char SPARE = '/';
-    private final char MISS = '-';
-    private final String BOUNDARY_NORMAL = "\\|";
-    private final String BOUNDARY_EXTRA = "\\|\\|";
+    private final static int  GAME_ROUND = 10;
+    private final static int  BOWLING_NUM = 10;
+    private final static char STRIKE = 'X';
+    private final static char SPARE = '/';
+    private final static char MISS = '-';
+    private final static String BOUNDARY_NORMAL = "\\|";
+    private final static String BOUNDARY_EXTRA = "\\|\\|";
+
     public int getBowlingScore(String bowlingCode) {
         int sumGoal = 0;
-        int left = BOWLING_NUM;
-        int fShot = 0;
-        int sShot = 0;
+        int fShot;
+        int sShot;
         String[] bowlingGrid = bowlingCode.split(BOUNDARY_EXTRA);
-        int extraNum = bowlingGrid.length - 1;
+        boolean extraRound = bowlingGrid.length > 1;
         String[] normalBowlingGrid = bowlingGrid[0].split(BOUNDARY_NORMAL);
         for (int i = 0; i < GAME_ROUND; i++) {
-            fShot = getGoal(left,normalBowlingGrid[i].charAt(0));
-            sShot = getGoal(left - fShot,normalBowlingGrid[i].charAt(1));
+            fShot = getGoal(BOWLING_NUM,normalBowlingGrid[i].charAt(0));
+            sShot = getGoal(BOWLING_NUM - fShot,normalBowlingGrid[i].charAt(1));
+            sumGoal += fShot + sShot;
+        }
+        if (extraRound){
+            fShot = getGoal(BOWLING_NUM,bowlingGrid[1].charAt(0));
+            if(bowlingGrid[1].length()>1){
+                sShot = getGoal(BOWLING_NUM - fShot,bowlingGrid[1].charAt(1));
+            } else {
+                sShot = 0;
+            }
             sumGoal += fShot + sShot;
         }
         return sumGoal;
