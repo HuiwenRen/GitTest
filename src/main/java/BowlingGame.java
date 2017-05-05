ede77faa8df6bba5343f617b4d6c8deb1103b022
Rewinder <ren2935872@126.com>
2017-05-05 16:04:31 +0800

    add | all method but I do not like it at all

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,43 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-    private final static int  GAME_ROUND = 10;
-    private final static int  BOWLING_NUM = 10;
+    private final static int GAME_ROUND = 10;
+    private final static int BOWLING_NUM = 10;
     private final static char STRIKE = 'X';
     private final static char SPARE = '/';
     private final static char MISS = '-';
     private final static String BOUNDARY_NORMAL = "\\|";
     private final static String BOUNDARY_EXTRA = "\\|\\|";
 
+    List<Grid> gridList = new ArrayList<Grid>();
+
+    class Grid {
+        boolean isExtra;
+        int left;
+        int getGoal;
+        int crack;
+        List<Integer> shot;
+
+        Grid(String code, boolean isExtra) {
+            this.isExtra = isExtra;
+            crack = 1;
+            getGoal = 0;
+            left = BOWLING_NUM;
+            shot = new ArrayList<Integer>();
+            for (int i = 0; i < code.length(); i++) {
+                shot.add(getGoal(left, code.charAt(i)));
+                if(!isExtra){
+                    int cra = 1;
+                    left -= shot.get(i);
+                    if (left == 0) {
+                        cra += 1;
+                    }
+                    if (shot.get(i) == BOWLING_NUM) {
+                        cra += 1;
+                    }
+                    getGoal = cra > 1 ? 10 : getGoal + shot.get(i) * crack;
+                    crack = cra;
+                }
+            }
+        }
+    }
+
+    private void initGridList(String bowlingCode) {
+        String[] bowlingGrid = bowlingCode.split(BOUNDARY_EXTRA);
+        String[] normalBowlingGrid = bowlingGrid[0].split(BOUNDARY_NORMAL);
+        boolean extraRound = bowlingGrid.length > 1;
+        for (int i = 0; i < GAME_ROUND; i++) {
+            gridList.add(new Grid(normalBowlingGrid[i], false));
+        }
+        if (extraRound) {
+            gridList.add(new Grid(bowlingGrid[1], true));
+        }
+    }
+
     public int getBowlingScore(String bowlingCode) {
         int sumGoal = 0;
-        int fShot;
-        int sShot;
-        String[] bowlingGrid = bowlingCode.split(BOUNDARY_EXTRA);
-        boolean extraRound = bowlingGrid.length > 1;
-        String[] normalBowlingGrid = bowlingGrid[0].split(BOUNDARY_NORMAL);
-        for (int i = 0; i < GAME_ROUND; i++) {
-            fShot = getGoal(BOWLING_NUM,normalBowlingGrid[i].charAt(0));
-            sShot = getGoal(BOWLING_NUM - fShot,normalBowlingGrid[i].charAt(1));
-            sumGoal += fShot + sShot;
-        }
-        if (extraRound){
-            fShot = getGoal(BOWLING_NUM,bowlingGrid[1].charAt(0));
-            if(bowlingGrid[1].length()>1){
-                sShot = getGoal(BOWLING_NUM - fShot,bowlingGrid[1].charAt(1));
-            } else {
-                sShot = 0;
+        int fCrack = 0;
+        int sCrack = 0;
+        initGridList(bowlingCode);
+        for (Grid grid : gridList) {
+            sumGoal += grid.getGoal;
+            sumGoal += grid.shot.get(0) * fCrack;
+            if(sCrack != 0 && grid.shot.size()>1){
+                sumGoal += grid.shot.get(1) * sCrack;
+                sCrack = 0;
             }
-            sumGoal += fShot + sShot;
+            fCrack = grid.crack > 1 ? sCrack + 1 : sCrack;
+            sCrack = grid.crack > 2 ? 1 : 0;
         }
         return sumGoal;
     }
 
-    private int getGoal(int left, char get){
-        switch(get){
-            case '-':return 0;
-            case 'X':return 10;
-            case '/': return left;
-            default:return get-'0';
+    private static int getGoal(int left, char sym) {
+        switch (sym) {
+            case MISS:
+                return 0;
+            case STRIKE:
+            case SPARE:
+                return left;
+            default:
+                return sym - '0';
         }
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -15,31 +15,37 @@ public class BowlingGameTest {
         String bowlingCode = "--|--|--|--|--|--|--|--|--|--||";           //0
         assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 0);
     }
-//
-//    @Test
-//    public void test_all_strike() throws Exception {
-//        String bowlingCode = "X|X|X|X|X|X|X|X|X|X||XX";           //300
-//        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 300);
-//    }
-//
-//    @Test
-//    public void test_second_spare() throws Exception {
-//        String bowlingCode = "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5";  //150
-//        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 150);
-//    }
-//
-//    @Test
-//    public void test_second_miss() throws Exception {
-//        String bowlingCode = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||";   //90
-//        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 90);
-//
-//    }
-//
-//    @Test
-//    public void test_other_situation() throws Exception {
-//        String bowlingCode = "X|7/|9-|X|-8|8/|-6|X|X|X||81";      //167
-//        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 167);
-//
-//    }
+
+    @Test
+    public void test_extra_round() throws Exception {
+        String bowlingCode = "12|23|34|45|54|43|32|21|11|1/||5";           //65
+        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 65);
+    }
+
+    @Test
+    public void test_all_strike() throws Exception {
+        String bowlingCode = "X|X|X|X|X|X|X|X|X|X||XX";           //300
+        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 300);
+    }
+
+    @Test
+    public void test_second_spare() throws Exception {
+        String bowlingCode = "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5";  //150
+        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 150);
+    }
+
+    @Test
+    public void test_second_miss() throws Exception {
+        String bowlingCode = "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||";   //90
+        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 90);
+
+    }
+
+    @Test
+    public void test_other_situation() throws Exception {
+        String bowlingCode = "X|7/|9-|X|-8|8/|-6|X|X|X||81";      //167
+        assertEquals(new BowlingGame().getBowlingScore(bowlingCode), 167);
+
+    }
 }
 
